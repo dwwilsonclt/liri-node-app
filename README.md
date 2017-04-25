30b3508e28fd372d236990c6e8c48283aca4b9bf
Dave Wilson <dwwilsonclt@icloud.com>
2017-04-25 18:02:11 -0400

    4-25 updaet

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # liri-node-app
 LIRI project WK10
 In this assignment, you will make LIRI. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+All liri execution logged into log.txt file 
+
+Test scripts follow:
+
+1	node liri  my-tweets
+2	node liri  my-tweets,blah
+3	node liri  spotify-this-song
+4	node liri  spotify-this-song,desperado
+5	node liri  spotify-this-song desperado
+6	node liri  movie-this
+7	node liri  movie-this the godfather
+8	node liri  do-what-it-says
+9	node liri  do-what-it-says,blah
+
+Expectation and validation of results:
+
+1	logging of recent tweets
+2	verify running after programatically removing improper user entry of parameter
+3	verify default of "The Sign" with no parameter entry
+4	verify running after programatically removing improper user entry of parameter; should run song desperado
+5	verify song desperado run
+6	verify OMDB "Mr Nobody" is run with no parameter entry
+7	verify OMDB the godfather
+8	verify runs random.txt with song "I Want it That Way"
+9	verify running after programatically removing improper user entry of parameter
+
+NOTE: VERIFICATION PROVIDED IN testvalidation.txt file 4-25-2017 - Approved by Davis
--- liri.js
+++ liri.js
@@ -1,67 +1,201 @@
+// LOAD NPM - TWITTER
 var Twitter = require('twitter');
 var keys = require("./keys.js");
 var config = keys.twitterKeys;
-// console.log(config)
 var client = new Twitter(config);
 
-// twitter.get('favorites/list', error, tweets, response) 
-
-// var params = {screen_name: 'cnn'};
-// twitter.get('statuses/user_timeline', params, error, tweets, response)
-
-// function error(err) {
-//     console.log(err);
-// };
-
-// function tweets(data) {
-//     console.log("tweets")
-//     console.log(data);
-// };
-// function response(data) {
-// 	console.log("response")
-//     console.log("response " + data);
-// };
-
-var params = {
-    q: '@JonahNRO'
-};
-
-client.get('search/tweets', params, function(error, tweets, response) {
-    if (!error) {
-        console.log(JSON.parse(response,null,2));
-        // console.log(JSON.stringify(response,null,2));
-    } else {
-        console.log(error);
-    }
-});
-
+// LOAD NPM - SPOTIFY
 var  spotify  =  require('spotify');
-var paramsSpotify = {
-    type: 'track',
-    query: 'the sign'
-} 
-spotify.search(paramsSpotify,  function(err,  data)  {    
-    if  ( err )  {         console.log('Error occurred: '  +  err);        
-        return;     }
-    var itemData = data.tracks.items
-        // console.log("items: " + itemData.length)
 
-    for (var i = itemData.length - 1; i >= 0; i--) {
-        var trackArtists = itemData[i].album.artists
-        var albumName = itemData[i].album.name;
-        console.log("Album name - " + albumName)
-        var previewLink = itemData[i].album.external_urls.spotify;
-        console.log("    Album preview link: " + previewLink)
-        for (var j = trackArtists.length - 1; j >= 0; j--) {
-            var trackArtist = trackArtists[j].name;
-            console.log("    Performed by artist - " + trackArtist)
+// LOAD NPM - REQUEST
+var request = require("request");
+
+//LOAD FS FOR readFile (if required)
+var fs = require("fs");
+
+//GLOBAL VARIABLES FOR PARAMETERS
+var paramEntered = "";
+var commandEntered = "";
+var allParams = process.argv.splice(2)
+
+//CHECK USER INPUT FOR ANOMOLIES RETURNING CLEAN commandEntered and paramEntered
+validateParameters(allParams);
+
+//EXECUTE COMMAND
+executeLIRI();
+
+//FUNCTION TO EXECUTE COMMAND PRESENTED
+function executeLIRI() {
+    // MAIN LOGIC FOLLOWS
+    switch (commandEntered) {   
+        case "my-tweets":
+                  twittersearch();       
+            break;   
+        case "spotify-this-song":
+                  spotifysearch();       
+            break;   
+        case "movie-this":
+                  omdbsearch();       
+            break;   
+        case "do-what-it-says":
+                  dowhatitsays();       
+            break;   
+        default:
+                  console.log("UNRECOGNIZED REQUEST - TRY AGAIN")
+    }
+}
+
+function omdbsearch() {
+    //OMDB SEARCH CODE FOLLOWS
+    var searchOMDB = "Mr.+Nobody";
+    if (paramEntered !== "") {
+        searchOMDB = paramEntered.replace(/ /g, "+")
+    }
+    var queryURLBase = "http://www.omdbapi.com/?t="
+    var queryURLParams = "&y=&plot=short&r=json"
+    var queryURL = queryURLBase + searchOMDB + queryURLParams;
+
+    request(queryURL, function(error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            console.log("========================")
+            console.log("OMDB SEARCH FOLLOWS:")
+            console.log("The movie's name is : " + JSON.parse(body).Title);
+            console.log(" Year movie came out : " + JSON.parse(body).Year);
+            console.log(" IMDB rating  : " + JSON.parse(body).imdbRating);
+            console.log(" Country produced : " + JSON.parse(body).Country);
+            console.log(" Language : " + JSON.parse(body).Language);
+            console.log(" Plot of the movie : " + JSON.parse(body).Plot);
+            console.log(" Actors : " + JSON.parse(body).Actors);
+            //LOG TO FILE
+            fs.appendFile("log.txt", "========================" + "\r\n", function() {});
+            fs.appendFile("log.txt", "OMDB SEARCH FOLLOWS:" + "\r\n", function() {});
+            fs.appendFile("log.txt", "The movie's name is : " + JSON.parse(body).Title + "\r\n", function() {});
+            fs.appendFile("log.txt", " Year movie came out : " + JSON.parse(body).Year + "\r\n", function() {});
+            fs.appendFile("log.txt", " IMDB rating  : " + JSON.parse(body).imdbRating + "\r\n", function() {});
+            fs.appendFile("log.txt", " Country produced : " + JSON.parse(body).Country + "\r\n", function() {});
+            fs.appendFile("log.txt", " Language : " + JSON.parse(body).Language + "\r\n", function() {});
+            fs.appendFile("log.txt", " Plot of the movie : " + JSON.parse(body).Plot + "\r\n", function() {});
+            fs.appendFile("log.txt", " Actors : " + JSON.parse(body).Actors + "\r\n", function() {});
+            var ratings = JSON.parse(body).Ratings;
+            for (var i = ratings.length - 1; i >= 0; i--) {
+                if (ratings[i].Source === "Rotten Tomatoes") {
+                    console.log(" Rotten Tomatoes Rating : " + ratings[i].Value);
+                    fs.appendFile("log.txt", " Rotten Tomatoes Rating : " + ratings[i].Value + "\r\n", function() {});
+
+                }
+            }
+
+            console.log(" Website : " + JSON.parse(body).Website);
+            fs.appendFile("log.txt", " Website : " + JSON.parse(body).Website + "\r\n", function() {});
 
         }
+    });
+}
+
+//TWITTER ACCESS CODE FOLLOWS
+function twittersearch() {
+    var params = {
+        q: 'BidDaddyCLT'
+    };
+
+    client.get('search/tweets', params, function(error, tweets, response) {
+        if (!error) {
+
+        } else {
+            console.log(error);
+        }
+        fs.appendFile("log.txt", "========================" + "\r\n", function() {});
+        fs.appendFile("log.txt", "TWEETS FOLLOW" + "\r\n", function() {});
+        console.log("========================");
+        console.log("TWEETS FOLLOW");
+        for (var i = tweets.statuses.length - 1; i >= 0; i--) {
+            console.log(tweets.statuses[i].created_at);
+            console.log("   " + tweets.statuses[i].text);
+            fs.appendFile("log.txt", tweets.statuses[i].created_at + "\r\n", function() {});
+            fs.appendFile("log.txt", "   " + tweets.statuses[i].text + "\r\n", function() {});
+        }
+    });
+}
+
+//SPOTIFY ACCESS CODE FOLLOWS
+function spotifysearch() {
+    var paramsSpotify = {
+        type: 'track',
+        query: ''
+    } 
+    var queryTrack = "The Sign"
+    if (paramEntered !== "") {
+        queryTrack = paramEntered;
     }
-    // console.log("data.tracks.items[0].album.artists[0].name")
-    // console.log(data.tracks.items[0].album.artists[0].name);
+    paramsSpotify.query = queryTrack;
+    spotify.search(paramsSpotify,  function(err,  data)  {    
+        if  ( err )  {        
+            console.log('Error occurred: '  +  err);        
+            return;    
+        }
+        var itemData = data.tracks.items
+        console.log("========================");
+        console.log("SPOTIFY FOLLOWS");
+        fs.appendFile("log.txt", "========================" + "\r\n", function() {});
+        fs.appendFile("log.txt", "SPOTIFY FOLLOWS" + "\r\n", function() {});
+        console.log("Song search - " + paramsSpotify.query)
+        fs.appendFile("log.txt", "Song search - " + paramsSpotify.query + "\r\n", function() {});
+        for (var i = itemData.length - 1; i >= 0; i--) {
+            var trackArtists = itemData[i].album.artists
+            var albumName = itemData[i].album.name;
+            console.log("Album name - " + albumName);
+            fs.appendFile("log.txt", "Album name - " + albumName + "\r\n", function() {});
+            var previewLink = itemData[i].album.external_urls.spotify;
+            console.log("    Album preview link: " + previewLink);
+            fs.appendFile("log.txt", "    Album preview link: " + previewLink + "\r\n", function() {});
+            for (var j = trackArtists.length - 1; j >= 0; j--) {
+                var trackArtist = trackArtists[j].name;
+                console.log("    Performed by artist - " + trackArtist);
+                fs.appendFile("log.txt", "    Performed by artist - " + trackArtist + "\r\n", function() {});
+            }
+        }
+    });
+}
 
+// RANDOM REQUEST CODE FOLLOWS
+// READ THE REQUEST AND EXECUTE THE COMMAND
+function dowhatitsays() {
+    console.log("DO WHAT IT SAYS===========================");
+    fs.appendFile("log.txt", "DO WHAT IT SAYS===========================");
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        var dataArr = data.split(",");
+        commandEntered = dataArr[0];
+        if (dataArr.length > 1) {
+            paramEntered = dataArr[1];
+        }
+        executeLIRI();
+    });
+}
 
-          // Do something with 'data' 
+//FUNCTION FOLLOWS TO "BULLET PROOF" USER PARAMETER ENTRY PROVIDING FUNCTIONS WITH CLEAN 'commandEntered' & 'paramEntered'
+function validateParameters(params) {
 
-});
+    var parsedParams = "";
+
+    //BUILD A STRING - first remove any stray commas (cleaning a parameter) AND seperating each parameter with a ','
+    for (var i = 0; i < params.length; i++) {
+        var str = params[i].split(",")
+        parsedParams += str.join() + ","
+        str = []
+    }
+
+    parsedParams = parsedParams.trim();
+
+    //NOW RE-BUILD THE ARRAY TO CONTAIN ALL PARAMETERS SANS ','
+    params = parsedParams.split(",")
+
+    //params[0] is the command
+    commandEntered = params[0];
+
+    //slice param[0] out of the array and join them seperated by a space
+    if (params.length > 1) {
+        params = params.slice(1);
+        paramEntered = params.join(" ");
+    }
+}
